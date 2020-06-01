0912cf55e4177a641e851d066af4fedb984b427c
steinko <stein_korsveien@msn.com>
2020-06-01 20:02:52 +0200

    Add bean config

--- component/src/main/java/org/steinko/bank/BeanConfig.java
+++ component/src/main/java/org/steinko/bank/BeanConfig.java
@@ -2,6 +2,9 @@ package org.steinko.bank;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.steinko.bank.bankaccount.BankAccountRepository;
+import org.steinko.bank.bankaccount.BankAccountService;
+import org.steinko.bank.bankaccount.BankAccountServiceImp;
 import org.steinko.bank.customer.CustomerRepository;
 import org.steinko.bank.customer.CustomerService;
 import org.steinko.bank.customer.CustomerServiceImp;
@@ -15,8 +18,17 @@ public class BeanConfig {
 	CustomerRepository customerRepository;
 	
 	@Bean
-	  CustomerService customerService() {
+	  public CustomerService customerService() {
 		return new CustomerServiceImp(customerRepository);
+		
+	}
+	
+	@Autowired 
+	BankAccountRepository bankAccountRepository;
+	
+	@Bean
+	  public BankAccountService bankAccountService() {
+		return new BankAccountServiceImp(bankAccountRepository);
 		
 	}
 }
